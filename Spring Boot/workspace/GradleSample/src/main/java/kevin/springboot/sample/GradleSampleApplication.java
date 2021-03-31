b71024b3dfe4694785fe69e7fac0a66e452eead7
Kevin Lee <kylee1112@hotmail.com>
2021-04-01 05:39:37 +0900

    banner 적용

--- Spring Boot/workspace/GradleSample/src/main/java/kevin/springboot/sample/GradleSampleApplication.java
+++ Spring Boot/workspace/GradleSample/src/main/java/kevin/springboot/sample/GradleSampleApplication.java
@@ -1,13 +1,16 @@
 package kevin.springboot.sample;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GradleSampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GradleSampleApplication.class, args);
+		SpringApplication app = new SpringApplication(GradleSampleApplication.class);
+		app.setWebApplicationType(WebApplicationType.NONE);
+	    app.run(args);
 	}
 
 }
