2b7e2e1b5017dc204b3c68d51fbc6790dd3914dd
Kevin Lee <kylee1112@hotmail.com>
2021-04-13 07:21:08 +0900

    HttpMessageConverters

--- Spring Boot/workspace/GradleSample/src/main/java/kevin/springboot/sample/mvc/WebMvcController.java
+++ Spring Boot/workspace/GradleSample/src/main/java/kevin/springboot/sample/mvc/WebMvcController.java
@@ -1,6 +1,8 @@
 package kevin.springboot.sample.mvc;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class WebMvcController {
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello, WebMvc";
+	}
+	
+	@PostMapping("/users/create")
+	public User create(@RequestBody User user) {
+		return user;
 	}
 }
